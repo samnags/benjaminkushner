495ee1223d67ecad66e6dd183d9bfd851f4522cd
samnags <sam.nagourney@gmail.com>
2017-05-22 09:58:26 -0700

    styling edits and page swap

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
     <link rel="stylesheet" type="text/css" href="vendors/css/normalize.css">        
     <link rel="stylesheet" type="text/css" href="vendors/css/ionicons.min.css">
     <link rel="stylesheet" type="text/css" href="resources/css/style.css">        
-    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">    
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+        
     
     <title>Benjamin M. Kushner | Workforce Development </title>
 </head>
@@ -20,8 +21,8 @@
                 <div class="row">
                     <h1 class="name">Benjamin M. Kushner</h1>        
                     <ul class="nav">
-                        <li><a href="#bio">About</a><li>
-                        <li><a href="#profile">Profile</a><li>
+                        <li><a href="#services">Services</a><li>
+                        <li><a href="#profile">About</a><li>
                         <li><a href="#contact">Contact</a><li>
                     </ul>     
                 </div>
@@ -29,52 +30,55 @@
 
             <div class="row">                
                 <div class="hero-text">                    
-                    Technical Assistance in Workforce Development, Capacity Building, and WIOA Implementation
+                    Collaborative strategist for workforce development challenges
                 </div>            
-          </div>
-          <div class="row image-caption">
-              Australia, February 2013
-          </div>            
+          </div>          
     </header>
 
-    <section id="profile" class="profile">
+    <section id="profile" class="profile js--section--profile">
         <div class="row">
-            <div class="col span-1-of-3 img-box">
+            <div class="col span-1-of-2 img-box">
                 <img src="./resources/images/headshotcrop.jpg" alt="">
             </div>
-            <div class="col span-1-of-3 image-text">                
-                Career Services<br>
-                Business Services<br>
-                AJC/One-Stop Operations<br>
-                WIOA Implementation<br>
-                Facilitation<br>
-                Career Pathways<br>
-                Sector Strategies<br>
-                Strategic Planning<br>                
-            </div>
-            <div class="col span-1-of-3 image-text">
-                Resource Mapping<br>
-                Reentry Employment<br>
-                Pre-Apprenticeship<br>
-                Registered Apprenticeship<br>
-                On-the-Job Training<br>
-                Reemployment<br>
-                Regionalism<br>
-            </div>
+            <div class="col span-1-of-2 long">
+                Ben Kushner has been a Workforce Analyst for almost 20 years.  During that time, he’s handled 
+    federal technical assistance contracts that are funded by the Workforce Innovation and Opportunity Act and by the Affordable Care Act. He is based in Los Angeles.<br> 
+                <br>
+                For a complete bio, <br><a href="resources/images/benjaminkushner-bio.pdf" target="_blank"><button type="button" class="btn btn-info">click here</button></a><br><br>
+                For a list of past projects and his resume, <br><a href="resources/images/benjaminkushner-resume.pdf" target="_blank"><button type="button" class="btn btn-info" >click here</button></a>                                           
+            </div>            
         </div>   
     </section>
 
-    <section id="bio" class="bio js--section--bio">
+    <section id="services" class="services">
         <div class="row">
-            <div class="col span-1-of-3 long">
-                Ben Kushner has been employed in career, education and workforce development since the establishment of the Workforce Investment Act in 1998.  After sixteen years as a commodities trader and marketing manager, he launched a career change, taking full advantage of the dislocated worker services offered at a WIA-funded program in New York City.  He was counseled by a team of career management professionals and then became one himself after receiving training and certification at New York University’s Center for Career, Education and Life Planning.  
+            <div class="col span-1-of-3">
+                <h2>Services</h2>
             </div>
-            <div class="col span-1-of-3 long">
-                In 2001, he went on to help establish the Business Services unit at New York City’s first One Stop Career Center.  From 2001 – 2003, Ben worked as a consultant for the New York City Human Resources Administration where he helped the agency establish programs that served people on public assistance and workers dislocated by the events of 9/11.  
+            <div class="col span-1-of-3">
+                <ul style="list-style-type:circle">
+                    <li>Career Services</li>
+                    <li>Business Services</li>
+                    <li>AJC/One-Stop Operations</li>
+                    <li>WIOA Implementation</li>
+                    <li>Facilitation</li>
+                    <li>Career Pathways</li>
+                    <li>Sector Strategies</li>
+                    <li>Strategic Planning</li>                
+                </ul>     
             </div>
-            <div class="col span-1-of-3 long">
-                Between 2003 and 2010, Ben was a contractor and later, a federal employee at ETA focusing on building the workforce system’s capacity, One Stop operations, reemployment strategies, technical assistance initiatives, and ETA’s web-based communications tools including Workforce3One (now WorkforceGPS) and the Communities of Practice. In 2010, Ben moved to Los Angeles where he has been working as an independent consultant on federal and state workforce development contracts. He earned his bachelor’s degree in Social Science at the State University of New York, College at Purchase. 
-            </div>                
+            <div class="col span-1-of-3">
+                <ul style="list-style-type:circle">
+                    <li>Resource Mapping</li>
+                    <li>Reentry Employment</li>
+                    <li>Pre-Apprenticeship</li>
+                    <li>Registered Apprenticeship</li>
+                    <li>On-the-Job Training</li>
+                    <li>Reemployment</li>
+                    <li>Regionalism</li>
+                </ul>
+                
+            </div>            
         </div>
     </section>
 
@@ -90,7 +94,7 @@
                                 echo "<div class=\"form-messages error\">Something went wrong! Please try again.</div>"; 
                             }
                             
-                        ?> 
+                        ?>
                     </div>
                     <div class="row">                        
                         <div class="col span-1-of-3">
@@ -127,6 +131,10 @@
                     
                 </form>
             </div>
+            <div>
+                <a class="designed" href="http://samnagourney.com" target="_blank">Designed and developed by Sam Nagourney</a>    
+            </div> 
+
     </section>
 
     <script src="vendors/js/jquery-3.2.1.min.js"></script>
--- resources/js/index.js
+++ resources/js/index.js
@@ -1,13 +1,11 @@
 $(document).ready(() => { 
 
-    $('.js--section--bio').waypoint(function(direction) {
+    $('.js--section--profile').waypoint(function(direction) {
         if(direction === 'down') {
             $('nav').addClass('sticky');
         } else {
             $('nav').removeClass('sticky');
         }
-    }, {
-        offset: '60px'
     })
 
     $.localScroll();
